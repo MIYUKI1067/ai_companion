fa159935b35e35465152e75f6da392afa7283b5f
MIYUKI1067 <superbrian2917@gmail.com>
2026-03-10 23:49:16 +0800

    add placeholder files

--- app/run_chat.py
+++ app/run_chat.py
@@ -6,9 +6,10 @@ from simulation.schedule_engine import get_current_activity
 
 from simulation.state_engine import update_mood, update_energy
 
+from simulation.event_engine import generate_event, apply_event
+
 
 character = json.load(open("characters/xuran/profile.json"))
-
 
 state = {
 
@@ -30,10 +31,17 @@ while True:
     state["energy"] = update_energy(state["energy"], state["activity"])
 
 
+    event = generate_event()
+
+    if event:
+
+        state = apply_event(state, event)
+
+        print(f"\n[生活事件] {event['name']}\n")
+
+
     msg = input("你：")
 
-
     reply = chat(msg, character, state)
-
 
     print("許然：", reply)
--- simulation/event_engine.py
+++ simulation/event_engine.py
@@ -2,23 +2,63 @@ import random
 
 EVENT_POOL = [
 
-("今天臨時被要求加班", -0.3, 0.3),
+{
+"name":"臨時加班",
+"activity":"在醫院加班",
+"mood":-0.2,
+"energy":-0.2
+},
 
-("和小芸一起吃晚餐", 0.3, -0.1),
+{
+"name":"和小芸吃晚餐",
+"activity":"和小芸吃飯",
+"mood":0.3,
+"energy":-0.1
+},
 
-("今天跑步跑得很舒服", 0.4, -0.2),
+{
+"name":"河濱跑步",
+"activity":"河濱跑步",
+"mood":0.4,
+"energy":-0.2
+},
 
-("被病人家屬感謝", 0.5, -0.1),
+{
+"name":"外拍工作",
+"activity":"外拍工作",
+"mood":0.2,
+"energy":-0.2
+},
 
-("拍攝取消了", -0.2, 0.1)
+{
+"name":"被病人家屬感謝",
+"activity":"在醫院工作",
+"mood":0.5,
+"energy":-0.1
+}
 
 ]
 
 
-def random_event():
+def generate_event():
 
     if random.random() < 0.2:
 
         return random.choice(EVENT_POOL)
 
     return None
+
+
+def apply_event(state,event):
+
+    state["activity"] = event["activity"]
+
+    state["mood"] += event["mood"]
+
+    state["energy"] += event["energy"]
+
+    state["mood"] = max(-1,min(1,state["mood"]))
+
+    state["energy"] = max(0,min(1,state["energy"]))
+
+    return state
